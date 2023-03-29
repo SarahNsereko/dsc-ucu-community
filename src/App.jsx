98e2c145ceac4fc979cb57dde27263c85f95a840
SarahNsereko <sarahnsereko59@gmail.com>
2023-03-29 18:48:36 +0300

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Signup from './pages/Signup/Signup'
+import Login  from './pages/Login/Login'
+import Home from './pages/Home/Home'
 import './App.css'
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/'>
-        <Route index element={<Signup/>} />
-        <Route path='login' element={<Login/>} /> 
-        <Route path='dashboard' element={<Dashboard/>} />
+      <Route exact path='/' element={<Home/>}/> 
       
-      </Route>
+      <Route path='/signup' element={<Signup/>} />
+        <Route path='/login' element={<Login/>} /> 
+        <Route path='/dashboard' element={<Dashboard/>} />
+      
     </Routes>
   </BrowserRouter>
   )
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Form from './Components/Form'
+import Form from '../../Components/Form'
 
 function Signup() {
   return (
