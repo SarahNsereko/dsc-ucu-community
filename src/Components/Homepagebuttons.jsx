207673a512159368dff264c8145f12b9d4e880cf
SarahNsereko <sarahnsereko59@gmail.com>
2023-03-31 11:17:00 +0300

    Changes to button group

--- src/Components/Homepagebuttons.jsx
+++ src/Components/Homepagebuttons.jsx
@@ -2,13 +2,11 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 
-
-export default function BasicButtonGroup() {
+export default function BasicButtonGroup(props) {
+  const { path, buttonLabel } = props;
   return (
     <ButtonGroup variant="contained" aria-label="outlined primary button group">
-      <Button href='/signup' >Signup</Button>
-      <Button href='/login'>Login</Button>
-     
+      <Button href={path}>{buttonLabel}</Button>
     </ButtonGroup>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BasicButtonGroup from "../../Components/signup_login_buttons";
+import BasicButtonGroup from "../../Components/Homepagebuttons";
+import './Home.css'
 
 function Home() {
     
@@ -16,25 +17,35 @@ function Home() {
     </div>
 
     <div className="Aboutgrid">
-    <div className="grid-item">
+      <div className="grid-item">
       <h2>About Us</h2>
       <p>Google Developer Student Clubs (DSCs) are community groups for university and college students interested in learning about Google technologies and developer tools. These clubs are open to any student, regardless of their academic background or major.</p>
-    </div>
-      <div className="grid-item">
+      </div>
+        <div className="grid-item">
        <h2>World Class Mentors</h2> 
        <p>Our Mentors are typically experienced professionals in the field of technology, and can offer valuable insights and expertise to students who are just starting out. DSCs are present in universities and colleges all around the world and are a great way for students to connect with like-minded individuals, learn new skills, and make an impact in their communities through technology.</p>
-        </div>
-      <div className="grid-item">
+          </div>
+            <div className="grid-item">
         <h2>Developer growth</h2>
         <p>Google Student Developer Programs provide opportunities for students to connect with other students, mentors, and professionals in the industry. These connections can help students learn about different career paths, gain valuable advice and insights, and even secure internships or job opportunities.</p>
-        </div>
-        <div className="grid-item">
+              </div>
+                <div className="grid-item">
           <h2>Want to join us?</h2>
-          <p>Take the first steps to joining a world winning team .If you havent registerd yet clik sign up .If you already have an account login</p>
-          <div className="signupandlogin">
-            <BasicButtonGroup/>
-          </div>
-        </div>
+          <p>Take the first steps to joining a world winning team .If you havent registerd yet clik sign up .Let's get to know you!</p>
+                    <div>
+                    <BasicButtonGroup path='/signup' buttonLabel='Signup' />
+                        </div>
+
+                </div>
+
+                <div className="grid-item">
+          <h2>Already a member?</h2>
+          <p>Welcome back!.Login and find out what's new. Find out what your team is up to and don't forget to write some lines of code today to build your project further!</p>
+                    <div>
+                    <BasicButtonGroup path='/login' buttonLabel='Login' />
+                        </div>
+
+                </div>
 
    </div>
 </div>
