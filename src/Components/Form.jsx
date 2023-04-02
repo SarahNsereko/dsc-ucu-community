e3cbbe6019c8ab33ba2c356226abcaa418ad1d83
SarahNsereko <sarahnsereko59@gmail.com>
2023-04-02 07:12:22 +0300

    Regex adjustments

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,23 +1,29 @@
-import "../Components/Form.css"
 import { useState } from "react";
+import "./Form.css";
 
-const Form=(props)=>{
-  const[focused,setFocused]=useState(false)
-  const {label ,errorMessage,onChange,id,...inputprops}=props;
-  const handleFocus=(e)=>{
-    setFocused(true)
-  }
-  
+const Form = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
-    <div className="forminput">
-      {/* <label>{label}</label> */}
-      <input className="input"{...inputprops} onChange={onChange} onBlur={handleFocus}
-      onFocus={()=>
-      inputprops.name==="confirmpassword" &&setFocused(true)}
-      focused={focused.toString()}/>
-      <span className="inputspan">{errorMessage}</span>
+    <div className="formInput">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
     </div>
- 
-  )
-}
+  );
+};
+
 export default Form;
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,103 +1,104 @@
-import React from "react";
-import Form from '../../Components/Form'
 import { useState } from "react";
-import './Signup.css'
-function Signup() {
-  const [values,setValues]=useState({
-    Fullname:"",
-    Email:"",
-    Phone:"",
-    Username:"",
-    Password:"",
-    ConfirmPassword:"",
+import "./Signup.css";
+import Form from "../../Components/Form";
 
-  })
+const Signup= () => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    fullname: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-  const inputs=[{
-    id:1,
-    name:'fullname',
-    type:'text',
-    placeholder:'Fullname',
-    errorMessage:"Please include a space between names", 
-    label:'Fullname',
-    required:true,
-    pattern:"john", /**include regex code here  */
+  const inputs = [
+    {
+      id: 1,
+      
+      name: "fullname",
+      type: "text",
+      placeholder: "Fullname",
+      label: "Fullname",
+    },
+     
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "Please provide a valid email address!",
+      label: "Email",
+      required: true,
+    },
 
-  },
-  {
-    id:2,
-    name:'email',
-    type:'email',
-    placeholder:'Email',
-    errorMessage:'Please provide a valid email!',
-    label:'Email',
-    required:true,
+    {
+      id: 3,
+      name: "phone",
+      type: "text",
+      placeholder: "Phone",
+      errorMessage: "Please provide a valid number!",
+      label: "Phone",
+      pattern:"^0[0-9]{9}$",
+      required: true,
+    },
+    {id:4,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      errorMessage:
+        "Username should have a capital letter at start and can have atmost 3 numbers in any position!",
+      label: "Username",
+      pattern:"^([A-Z][a-z]*)(\\d*[a-zA-Z]?){0,3}$",
+      required: true,
+    },
+    
+    {
+      id: 5,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+    {
+      id: 6,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      errorMessage: "Passwords don't match!",
+      label: "Confirm Password",
+      pattern: values.password,
+      required: true,
+    },
+  ];
 
-  },
-  {
-    id:3,
-    name:'phone',
-    type:'text',
-    placeholder:'Phone',
-    errorMessage:"Please provide a valid phone number!",
-    label:'Phone',
-    required:true,
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-  },
-  {
-    id:4,
-    name:'username',
-    type:'text',
-    placeholder:'Username',
-    errorMessage:"Username already in use!",
-    label:'Username',
-    required:true,
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
-  },
-  {
-    id:5,
-    name:'password',
-    type:'password',
-    placeholder:'Password',
-    errorMessage:"Password must contain a number, special character and uppercase letter or letters!",
-    label:'Password',
-    required:true,
-
-  },
-  {
-    id:6,
-    name:'confirmpassword',
-    type:'password',
-    placeholder:'ConfirmPassword',
-    errorMessage:"Passwords don't match",
-    label:'ConfirmPassword',
-    required:true, 
-
-  },
-]
-
-  const handleSubmit=(e)=>{
-    e.preventDefault(); /*stops page from refreshing*/
-  }
-  const onChange=(e)=>{/**onChange is a function that is called everytime an input fields value changes, contains event object e which has dta about the event which has happened */
-    setValues({...values,[e.target.name]:e.target.value})/** updates the values state object with the new values from any input fields that are changed. */
-  }
-
-  console.log(values)
   return (
-
-    <div className="Signup">
-        <form className="signupform" onSubmit={handleSubmit}>
-             <h1>Sign-up</h1>
-              {inputs.map((input)=>(
-                <Form key={input.id} {...input} value={values[input.name]} onChange={onChange} />
-              ))}
-                
-            <button className="Submit1">Submit</button>
-            </form>
+    <div className="signup">
+      <form  className="signupform" onSubmit={handleSubmit}>
+        <h1 className="heading1">Sign-Up</h1>
+        {inputs.map((input) => (
+          <Form
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button className="signupbutton">Submit</button>
+      </form>
     </div>
-   
   );
-}
+};
 
-export default Signup
+export default Signup;
