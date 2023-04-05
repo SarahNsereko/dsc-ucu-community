b25cab2bd7b64c7b49d8bf840d2eecc6ab9f1244
SarahNsereko <sarahnsereko59@gmail.com>
2023-04-05 12:37:23 +0300

    User sign in and sign up

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,127 +1,62 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        GDSC
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+const SignIn = () => {
+  const handleSubmit = (values, { setSubmitting, setErrors }) => {
+    const users = JSON.parse(localStorage.getItem('users')) || [];
 
-const theme = createTheme();
+    const user = users.find(u => u.email === values.email && u.password === values.password);
 
-export default function SignInSide() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    if (!user) {
+      setErrors({ password: 'Invalid email or password' });
+    } else {
+      // Perform the necessary actions when authentication succeeds, such as redirecting to another page or setting an authentication token
+      console.log('User authenticated');
+    }
+
+    setSubmitting(false);
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="./Signup" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-              <Copyright sx={{ mt: 5 }} />
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+    <div>
+      <h1>Sign In</h1>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          if (!values.password) {
+            errors.password = 'Required';
+          }
+          return errors;
+        }}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+             <label htmlFor="email">Email</label>
+            <Field type="email" name="email" />
+           
+            <ErrorMessage name="email" component="div"  className='error'/>
+
+            <label htmlFor="password">password</label>
+            <Field type="password" name="password" />
+           
+            <ErrorMessage name="password" component="div"  className='error'/>
+            <button type="submit" disabled={isSubmitting}>
+              Sign-in
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
-}
+};
+
+export default SignIn;
---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import './Signup.css';
+const SignupForm = () => (
+  <div>
+    <h1>Signup</h1>
+    <Formik
+      initialValues={{ fullName: '', phone: '', email: '', password: '', confirmPassword: '' }}
+      validate={values => {
+        const errors = {};
+      
+        // Full Name validation
+        if (!values.fullName) {
+          errors.fullName = 'Required';
+        } else if (!/^[\w]+[\s][\w]+$/.test(values.fullName)) {
+          errors.fullName = 'Must be two words separated by a space';
+        }
+
+        // email validation 
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+      
+        // Phone number validation
+        if (!values.phone) {
+          errors.phone = 'Required';
+        } else if (!/^07\d{8}$/.test(values.phone)) {
+          errors.phone = 'Must be 10 digits starting with 07';
+        }
+      
+        // Password validation
+        if (!values.password) {
+          errors.password = 'Required';
+        } else if (values.password.length < 8) {
+          errors.password = 'Must be at least 8 characters';
+        } else if (!/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/.test(values.password)) {
+          errors.password = 'Must contain at least one number and one special character';
+        }
+      
+        if (values.password !== values.confirmPassword) {
+          errors.confirmPassword = 'Passwords do not match';
+        }
+      
+        return errors;
+      }}
+      
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+          resetForm();
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <label htmlFor="fullName">Full Name:</label>
+          <Field type="text" name="fullName" id="fullName" />
+          <ErrorMessage name="fullName" component="div" className="error" />
+
+          <label htmlFor="phone">Phone:</label>
+          <Field type="text" name="phone" id="phone" />
+          <ErrorMessage name="phone" component="div" className="error"  />
+
+          <label htmlFor="email">Email:</label>
+          <Field type="email" name="email" id="email" />
+          <ErrorMessage name="email" component="div" className="error"  />
+
+          <label htmlFor="password">Password:</label>
+          <Field type="password" name="password" id="password" />
+          <ErrorMessage name="password" component="div" className="error"  />
+
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <Field type="password" name="confirmPassword" id="confirmPassword" />
+          <ErrorMessage name="confirmPassword" component="div" className="error" />
+
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+
+export default SignupForm;
