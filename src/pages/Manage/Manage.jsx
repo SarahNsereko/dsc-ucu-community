0de3c3b4826c0cbd601dbc8f9c86cd4127a8410a
SarahNsereko <sarahnsereko59@gmail.com>
2023-04-13 17:43:46 +0300

    Management page

--- src/pages/Manage/Manage.jsx
+++ src/pages/Manage/Manage.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Clipped from '../../Components/Clipped'
 
 const Manage= () => {
-  // Component logic here
+
 
   return (
-    // JSX markup here
+  
     <div>
      <Clipped/>
+     <p>This is the management page </p>
     </div>
   );
 };
